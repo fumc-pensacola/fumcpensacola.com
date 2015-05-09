3befa5d805fe74e1d480120d050cf296f9e20364
Andrew Branch <andrewbranch@mail.com>
2015-05-09 16:55:58 -0700

    Customize config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', getenv('DB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -53,6 +53,11 @@ define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
 
+/** Set custom constants */
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_DIR', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+// define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME']);
+
 /**
  * WordPress Database Table prefix.
  *
@@ -68,7 +73,7 @@ $table_prefix  = 'wp_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', getenv('WP_ENV') == 'development');
 
 /* That's all, stop editing! Happy blogging. */
 
